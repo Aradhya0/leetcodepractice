272caf7590e82ab0738a57947722b349e609b081
Aradhya Kumar <100125236+Aradhya0@users.noreply.github.com>
2023-02-18 20:12:18 +0530

    container with most water in cpp

---
+++ container with most water.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int i=0, j=height.size() -1;
+        int ans=0;
+        while(i<j)
+        {
+            int curr=(j-i)*min(height[i],height[j]);
+            ans = max(ans,curr);
+            if(height[i]<height[j])
+            i++;
+            else
+            j--;
+            
+        }
+        return ans;
+    }
+};
