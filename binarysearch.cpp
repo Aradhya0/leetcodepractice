b36f5553c1856f2cdc7c904dad75a8b559f717b0
Aradhya Kumar <100125236+Aradhya0@users.noreply.github.com>
2023-03-28 20:51:32 +0530

    binary search in cpp

---
+++ binarysearch.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int search(vector<int>& arr, int x) {
+         int n=arr.size();
+        int l=0;
+        int r=n-1;
+             while (l <= r) {
+        int m = l + (r - l) / 2;
+ 
+        // Check if x is present at mid
+        if (arr[m] == x)
+            return m;
+ 
+        // If x greater, ignore left half
+        if (arr[m] < x)
+            l = m + 1;
+ 
+        // If x is smaller, ignore right half
+        else
+            r = m - 1;
+    }
+ 
+    // if we reach here, then element was
+    // not present
+    return -1;
+        
+    }
+};
