996a52ee399ad5d4ce3b586dee7bd6c7f0cc003d
Aradhya Kumar <100125236+Aradhya0@users.noreply.github.com>
2023-04-06 20:07:28 +0530

    can make arithmetic prog from sequence in cpp

---
+++ canmakearithematicprogressionfromsequence.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool canMakeArithmeticProgression(vector<int>& arr) {
+         sort(arr.begin(), arr.end());
+        if(arr.size()==2) 
+            return true;
+        int c= arr[0]-arr[1];
+        for(int i=1;i<arr.size()-1;i++){
+            if((arr[i]-arr[i+1])!=c ) 
+                return false;
+        }
+        return true;
+        
+    }
+};
