157833464b51963bd969d11c7157b4c9dee113cb
Aradhya Kumar <100125236+Aradhya0@users.noreply.github.com>
2023-04-27 22:20:51 +0530

    Optimal partition of a string in cpp

---
+++ optimalprtitionofstring.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int partitionString(string str) {
+        int ans = 1;
+        set<char> s;
+        for(auto c: str)
+        {
+            if(s.find(c)!=s.end())
+            {
+                s.clear();
+                ans++;
+            }
+            s.insert(c);
+        }
+        return ans;
+    }
+};
