ee5119d48b70084cc9a97d63745a7a36b7c46b1c
Aradhya Kumar <100125236+Aradhya0@users.noreply.github.com>
2023-06-15 19:59:26 +0530

    validate binary search in cpp

---
+++ vlidtebinryserch.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    TreeNode* prev = NULL;
+    bool isValidBST(TreeNode* root) {
+        if(root==NULL) return true;
+        if(isValidBST(root->left)==false) return false;
+        if( prev!=NULL && root->val <= prev->val) return false;
+        prev = root;
+        return isValidBST(root->right);
+    }
+};
